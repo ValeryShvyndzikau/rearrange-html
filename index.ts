2dab216d2eaf5fa604526c41abb30bdd20166321
ValeryShvyndzikau <valery_shvyndzikau@epam.com>
2020-04-02 14:19:41 +0300

    2 files modified

--- index.ts
+++ index.ts
@@ -1,7 +1,3 @@
-// Import stylesheets
-import './style.css';
-
 import {map, reduce, forEach, entries} from 'lodash';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
+
